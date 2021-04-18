9abc21439705a5a54f635a0ea8277734b9fff890
SJMQ <sjmccue@hotmail.co.uk>
2021-04-18 18:51:11 +0100

    add edit, delete paths, error messages and tidy listing

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'rails', '~> 6.1.2', '>= 6.1.2.1'
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
+gem 'bootstrap-sass', '~> 3.3.6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 5.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController 
+
+  def show 
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #byebug
+    if @article.save
+      flash[:notice] = "Article was created successfully" 
+      redirect_to (@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'    
+    end 
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    #if @article.destroy(params.require(:article).permit(:title, :description))
+      #flash[:notice] = "Article was deleted successfully"
+      #redirect_to @article
+    #else
+      #render 'index'
+    #end  
+  end
+end
